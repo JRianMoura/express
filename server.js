cf925b1cf9edeab46f0d82fde3f0c561c3a15dcb
Judson Rian <judson_@outlook.com>
2023-10-29 13:18:25 -0300

    fazendo conexão com o banco de dados

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+require('dotenv').config();
 const { log } = require('console');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    console.log('Connected to MongoDB');
+    app.emit("pronto");
+}).catch( e => console.log(e));
+
 const routes = require('./routes');
 const path = require('path');
 const { middlewareGlobal, outroMiddleware }  = require('./src/middlewares/middleware')
@@ -14,8 +23,10 @@ app.set('view engine', 'ejs');
 
 // meu proprio middleware
 app.use(middlewareGlobal);
-app.use(outroMiddleware);
 app.use(routes);
-app.listen(3000, () => {
-    console.log('Acessar http://localhost:3000');
+
+app.on('pronto', () => {
+    app.listen(3000, () => {
+        console.log('Acessar http://localhost:3000');
+    });
 });
---
+++ src/models/HomeModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const HomeSchema = new mongoose.Schema({
+    título: { type: String, required: true },
+    descricao: String
+});
+
+const HomeModel = mongoose.model('Home', HomeSchema);
+
